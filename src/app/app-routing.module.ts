631c16897a7f98fab9002a65d7f1866bad001d71
Cormac Ó hAodhagáin <cormac.ohagan@hotmail.co.uk>
2019-10-06 01:23:32 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductAddComponent } from './Components/product-add/product-add.component';
+import { ProductEditComponent } from './Components/product-edit/product-edit.component';
+import { ProductGetComponent } from './Components/product-get/product-get.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product/create',
+    component: ProductAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: ProductEditComponent
+  },
+  {
+    path: 'products',
+    component: ProductGetComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/products'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './Components/product-add/product-add.component';
+import { ProductGetComponent } from './Components/product-get/product-get.component';
+import { ProductEditComponent } from './Components/product-edit/product-edit.component';
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductGetComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
